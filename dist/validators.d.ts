88d12febd0ddcd803544e64200ba10d1b8c66193
Alexander Whitaker <whitaker.alexander@gmail.com>
2019-03-15 20:40:09 -0500

    version 0.1.3

--- dist/validators.d.ts
+++ dist/validators.d.ts
@@ -2,4 +2,3 @@ export declare type Validator = {
     getError: (value: any) => string;
     validate: (value: any) => boolean;
 };
-export declare const required: Validator;
--- dist/validators.js
+++ dist/validators.js
@@ -1,11 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.required = {
-    getError: function (value) {
-        return 'This field is required.';
-    },
-    validate: function (value) {
-        return true;
-    }
-};
 //# sourceMappingURL=validators.js.map
